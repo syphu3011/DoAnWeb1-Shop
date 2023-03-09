c5b4d1f6cca5c5cfb8c1280f95ae59c575e48413
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-03-09 16:34:47 +0700

    cm-090323

--- init.js
+++ init.js
@@ -1,19 +1,19 @@
-var xhttp = new XMLHttpRequest();
-var data;
+// var xhttp = new XMLHttpRequest();
+// let data;
 // Gửi yêu cầu đến file PHP với giá trị của biến id là 1
-xhttp.open("GET", "init.php?id=1", true);
-xhttp.send();
-//Xử lý dữ liệu
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    // console.log(this.responseText);
-    data = JSON.parse(this.responseText);
-    // var data = this.responseText;
-    // data.forEach((element) => {
-    //   console.log(element.id, "\t", element.name);
-    // });
-    localStorage.setItem("data", JSON.stringify(data));
-    console.log(data);
-  }
-};
-console.log(data);
+// xhttp.open("GET", "init.php?id=1", true);
+// xhttp.send();
+// //Xử lý dữ liệu
+// xhttp.onreadystatechange = function () {
+//   if (this.readyState == 4 && this.status == 200) {
+//     // console.log(this.responseText);
+//     data = JSON.parse(this.responseText);
+//     // var data = this.responseText;
+//     // data.forEach((element) => {
+//     //   console.log(element.id, "\t", element.name);
+//     // });
+//     localStorage.setItem("data", JSON.stringify(data));
+//     console.log(data);
+//   }
+// };
+// console.log(data);
