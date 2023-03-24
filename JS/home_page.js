fe28278412367f774bfab23e86e4ffdb068172c0
ThaoMinh2907 <nguyenminhthao12b1@gmail.com>
2023-03-24 10:43:14 +0700

    cm-240323

---
+++ JS/home_page.js
@@ -0,0 +1,261 @@
+function get_data(res) {
+  //Loại sản phẩm
+  data.largeClassify = res.largeClassify;
+  //Màu
+  data.color = res.color;
+  //Kích cỡ
+  data.size = res.size;
+  //Sản phẩm
+  data.product = res.product;
+  //Sản phẩm trong kho
+  data.product_in_stock = res.product_in_stock;
+  //Khuyến mãi
+  data.promote = res.promotion;
+  //Ảnh sản phẩm
+  data.image_product = res.image_product;
+  //Danh sách sản phẩm
+  data.product_list = res.product_list;
+}
+var xhttp = new XMLHttpRequest();
+//load trang chủ
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(this.responseText); // lưu phản hồi vào biến cục bộ
+    // sử dụng biến response ngay tại đây
+    get_data(response);
+    create_Homepage(response);
+    // createHomepage();
+  }
+};
+xhttp.open("GET", "server/homepage.php", true);
+xhttp.send();
+console.log(data);
+// data = JSON.parse(localStorage.getItem("data"));
+function create_Homepage(data_res) {
+  //Theo loại
+  data_res.largeClassify.forEach((element) => {
+    var str = "";
+    //Sản phẩm trong loại
+    let data_product = data_res.product;
+    let count_i = 0;
+    for (let k = 0; k < data_product.length; k++) {
+      if (count_i == 5) {
+        break;
+      } else if (data_product[k].id.indexOf(element.id) != -1) {
+        count_i++;
+        let link_image = "";
+        let price_product = "";
+        //Link image
+        for (let i = 0; i < data_res.image_product.length; i++) {
+          if (data_res.image_product[i].id_product == data_product[k].id) {
+            link_image = data_res.image_product[i].link_image;
+          }
+        }
+        //Giá sản phẩm
+        for (let i = 0; i < data_res.product_list.length; i++) {
+          if (data_res.image_product[i].id_product == data_product[k].id) {
+            price_product = calculated(data_res.product_list[i].price) + " VND";
+          }
+        }
+        //danh sách sản phẩm
+        str +=
+          `
+      <li class="main_list_product_product" id="` +
+          data_product[k].id +
+          `">
+        <img class="main_list_product_product_image"
+          style=""
+          src="` +
+          link_image +
+          `"alt=""
+        />
+       <div class="main_list_product_product_infor"> 
+       <label class="product_infor_name">` +
+          data_product[k].name +
+          `</label>
+         <label>` +
+          price_product +
+          `</label>
+        </div>
+      </li>
+      `;
+      }
+    }
+    // element.
+    document.getElementById("main").innerHTML +=
+      `<div class="main_product">
+        <div class="main_name_class_product">` +
+      element.name +
+      `
+        </div>
+        <ul class="main_list_product">
+        ` +
+      str +
+      `
+        </ul>
+      </div>`;
+  });
+  //Bắt sk click sản phẩm
+  let click_product = document.getElementsByClassName(
+    "main_list_product_product"
+  );
+  for (let i = 0; i < click_product.length; i++) {
+    click_product[i].onclick = function () {
+      //   console.log(1);
+      //chọn xem sản phẩm
+      click_Product(click_product[i].id);
+    };
+  }
+}
+function click_Product(id) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log(response.data);
+        if (response.success) {
+          //-data -status
+          //data: -image_product:
+          //        +id_product
+          //        +link_image
+          //        +nname_image
+          //      -product:
+          //        +description
+          //        +id
+          //        +idstatus
+          //        +madein
+          //        +name
+          //      -madein_product
+          //        +id
+          //        +name
+          //      -attribute_product
+          //        +id_color
+          //        +id_size
+          //        +price
+          //
+          //Hiển thị
+          document.getElementById("show_product").style.display = "flex";
+          //inner vào
+          document.getElementById("show_product").innerHTML =
+            `<div class=sp_popup>
+              <div class="popup_left">
+                <img id="img_show"style="
+                  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+                " src="` +
+            //source ảnh
+            response.data.image_product[0].link_image +
+            `"/>
+                <div style="height:120px;
+                  display: block; overflow: hidden;box-sizing: border-box;">
+                  <ul id="review_image">
+                  </ul>
+                </div>
+              </div>
+              <div class="poup_right">
+                <label class="popup_name_product">` +
+            //tên sản phẩm
+            response.data.product[0].name +
+            `</label>
+                
+                  <label style="padding: 10px;display: block;">
+                    Xuất xứ: \t ` +
+            //xuất xứ sản phẩm
+            response.data.madein_product[0].name +
+            `
+                  </label>
+                <label style="padding: 10px;display: block;">
+                  Mô tả:
+                </label>       
+                <div style="padding: 10px;
+                  display: block;
+                  border: 1px solid gray;
+                  margin: 10px;
+                  height: 150px;
+                  overflow-y: scroll;">
+                  ` +
+            //mô tả sản phẩm
+            response.data.product[0].description +
+            `
+                </div>   
+                    <div id="select_size">
+                        <label style="margin-right: 10px;">Size</label>
+                    </div>
+                    <div id="select_color">
+                        <div class="item_color">
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+          //
+          //thêm ảnh vào phần review_image
+          response.data.image_product.forEach((element) => {
+            document.getElementById("review_image").innerHTML +=
+              `
+            <li class="list_image_review">
+              <img src="` +
+              //source
+              element.link_image +
+              `"/>
+            </li>
+            `;
+          });
+          //
+
+          //review ảnh
+          let li_img = document.getElementsByClassName("list_image_review");
+          li_img[0].style.borderColor = "red";
+          for (let i = 0; i < li_img.length; i++) {
+            li_img[i].onmouseenter = function () {
+              //đưa tất cả về borderColor black
+              enter_image(li_img);
+              //gán ảnh chọn về borderColor black
+              li_img[i].style.borderColor = "red";
+              //gắn source, show lên
+              document.getElementById("img_show").src =
+                li_img[i].getElementsByTagName("img")[0].src;
+            };
+          }
+          //
+          function enter_image(li) {
+            for (let i = 0; i < li.length; i++) {
+              li[i].style.borderColor = "black";
+            }
+          }
+          //inner size
+          for (let i = 0; i < response.data.attribute_product.length; i++) {
+            att = response.data.attribute_product[i];
+            //inner các button size
+            document.getElementById("select_size").innerHTML +=
+              `
+            <div class="item_size">
+            ` +
+              att.id_size.substring(3, att.id_size.length - 1) +
+              `
+            </div>
+            `;
+          }
+          let button_size = document.getElementsByClassName("item_size");
+          //
+          button_size[0].style.borderColor = "red";
+          for (let index = 0; index < button_size.length; index++) {
+            // const element = array[index];
+            button_size[index].onclick = function () {
+              enter_image(button_size);
+              button_size[index].style.borderColor = "red";
+            };
+          }
+        } else {
+          // Thông báo thất bại
+          alert("Không tìm thấy sản phẩm này!");
+        }
+      } else {
+        // Thông báo lỗi nếu có
+        alert("Lỗi khi kết nối đến server!");
+      }
+    }
+  };
+  xhr.open("POST", "./Server/get_product.php");
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("id_product=" + id);
+}
--- Server/get_product.php
+++ Server/get_product.php
@@ -5,6 +5,7 @@
     $status_product=false;
     //Lấy dữ liệu từ Database
     require_once("../init.php");
+    //id, name, madein, description, idstatus
     $sql="SELECT *  FROM product WHERE id= '$idproduct'";
     $stmt=$conn->prepare($sql);
     $stmt->execute();
@@ -13,15 +14,23 @@
         $stmt=null;
         $status=true;
         $id_country=$product["product"][0]["madein"];
-        $sql="SELECT *  FROM image_product WHERE id_product= '$idproduct'";
+        //link_image, name_image
+        $sql="SELECT link_image, name_image  FROM image_product WHERE id_product= '$idproduct'";
         $stmt=$conn->prepare($sql);
         $stmt->execute();
         $product["image_product"]=$stmt->fetchAll(PDO::FETCH_ASSOC);
         $stmt=null;
-        $sql="SELECT *  FROM input_country WHERE id= '$id_country'";
+        //name
+        $sql="SELECT ic.name  FROM input_country ic WHERE id= '$id_country'";
         $stmt=$conn->prepare($sql);
         $stmt->execute();
         $product["madein_product"]=$stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt=null;
+        //id_size, price,id_color
+        $sql="SELECT id_size, price,id_color FROM product_list WHERE id_product= '$idproduct'";
+        $stmt=$conn->prepare($sql);
+        $stmt->execute();
+        $product["attribute_product"]=$stmt->fetchAll(PDO::FETCH_ASSOC);
         $stmt=null;
     }
     // $response;
