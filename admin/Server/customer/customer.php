58e69fafbc7b493f4a7e4f70a0496a89e348729f
jinshinvn <thinhnguyen.280202@gmail.com>
2023-04-23 15:57:30 +0700

    get customer

--- admin/Server/customer/customer.php
+++ admin/Server/customer/customer.php
@@ -1,31 +1,63 @@
 <?php
-require_once('../../../init.php');
-require_once('../__class__/Table.php');
-require_once('../__class__/ReqHandling.php');
+require_once("../../../init.php");
+require_once("../__class__/Table.php");
+require_once("../__class__/ReqHandling.php");
 
-header('Content-Type: application/json; charset=utf-8');
+header("Content-Type: application/json; charset=utf-8");
 
 
 // * get information on child and parent table (use inner join)
-if ($_SERVER['REQUEST_METHOD'] === "GET"){	
+if ($_SERVER["REQUEST_METHOD"] === "GET"){	
 	// * GET
+	
+	$cusHeader = Table::describe($conn, "customer");
+	$accHeader = Table::describe($conn, "account");
+	$condition = "1=1";
+
+	foreach($_GET as $key => $value) {
+		if (in_array($key, $cusHeader))
+			$condition .= " AND customer.$key = '$value'";
+		if (in_array($key, $accHeader))
+			$condition .= " AND account.$key = '$value'";
+	}
+
+	if ($condition === "1=1" && isset($_GET['search'])) {
+		$toSearch = $_GET['search'];
+		$condition = "";
+		foreach($cusHeader as $key => $value) {
+			$condition .= " OR customer.$value = '$toSearch'";
+		}
+		foreach($accHeader as $key => $value) {
+			$condition .= " OR account.$value = '$toSearch'";
+		}
+		$condition = substr($condition, 4);
+	}
+
+	// echo $condition;
+
 	// ? http://localhost/doan/admin/Server/customer/customer.php
 	echo Table::jsonifyCouple(
 		$conn, 
 		Table::tableQueryCouple(
-			$conn, 
 			// * connection
-			'customer', 
+			$conn, 
 			// * childtable
-			'account', 
+			"customer", 
 			// * parenttable
-			'id_user',
+			"account", 
 			// * foreign key on child
-			'id_user',
+			"id_user",
 			// * foreign key on parent
-			'*'), 
-		'customer', 'account'
+			"id_user",
+			// * column to select
+			"*",
+			// * condition to select
+			$condition
+			// "customer.id="KH001""
+		), 
+		"customer", "account"
 	);
+	
 }
 
 
@@ -35,13 +67,13 @@ if ($_SERVER['REQUEST_METHOD'] === "GET"){
 
 // * UPDATE parent table
 // ? http://localhost/doan/admin/Server/customer/customer.php?id_user=USR013&username=cicada&password=cicada3303&action=update&privilege=sales&status=active
-// * use both USR and KH in id, don't use id_user
+// * use both USR and KH in id, don"t use id_user
 
 
 // * UPDATE child table
 // ? http://localhost/doan/admin/Server/customer/customer.php?action=update&name=cicada4&id=KH005
 
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	if (isset($_REQUEST["action"])){
 		
 		// * UPDATE EXISTED CUSTOMERS
@@ -52,11 +84,11 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
 			// * fetch all table properties on account & customer table 
 
-			$headerArrAccount = Table::describe($conn, 'account');
-			$headerArrCustomer = Table::describe($conn, 'customer');
+			$headerArrAccount = Table::describe($conn, "account");
+			$headerArrCustomer = Table::describe($conn, "customer");
 
-			date_default_timezone_set('Asia/Ho_Chi_Minh');
-			$today = gmdate('Y-m-d H:i:s', time());
+			date_default_timezone_set("Asia/Ho_Chi_Minh");
+			$today = gmdate("Y-m-d H:i:s", time());
 			// * Y : year with 4 digits
 			// * y : year with 2 digits
 			// * m : month with 2 digits
@@ -64,24 +96,24 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 			// * H : format 24h
 			// * h : format 12h
 			$_REQUEST["date_created"] = $today;
-			
+
 			// * if "id" value in POST request start with USR, update the parent table (account table)
-			if (isset($_REQUEST["id_user"]) && preg_match('/^USR/', $_REQUEST["id_user"])) 
+			if (isset($_REQUEST["id_user"]) && preg_match("/^USR/", $_REQUEST["id_user"])) 
 				foreach($headerArrAccount as $key => $value) 
 					if (isset($_REQUEST[$value]))
 					ReqHandling::updateDbOnProperty(
-						$conn, 'account', $value, $_REQUEST[$value], 'id_user', $_REQUEST["id_user"]
+						$conn, "account", $value, $_REQUEST[$value], "id_user", $_REQUEST["id_user"]
 					);
 
 			// * if "id" value in POST request start with KH, update the child table (customer table)
-			if (isset($_REQUEST["id"]) && preg_match('/^KH/', $_REQUEST["id"])) 
+			if (isset($_REQUEST["id"]) && preg_match("/^KH/", $_REQUEST["id"])) 
 				foreach($headerArrCustomer as $key => $value) 
 					if (isset($_REQUEST[$value]))
 						ReqHandling::updateDb(
-							$conn, 'customer', $_REQUEST["id"], 
-							$value, $_REQUEST[$value] 
+							$conn, "customer", $_REQUEST["id"], $value, $_REQUEST[$value] 
 						);
-			
+
+
 		}
 
 		// * CREATE NEW CUSTOMER
@@ -95,17 +127,18 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 				exit();
 			}
 
-			$maxIdAccount = Table::getMaxId($conn, 'account', 'id_user');
-			$maxIdCustomer = Table::getMaxId($conn, 'customer', 'id');
+			$maxIdAccount = Table::getMaxId($conn, "account", "id_user");
+			$maxIdCustomer = Table::getMaxId($conn, "customer", "id");
 
 			// * add new account id on REQUEST superglobal array
 			$_REQUEST["id_user"] = "USR" . strval(sprintf("%03d", $maxIdAccount+1));
 			// * update account table first (update parent table)
-			ReqHandling::createRow($conn, 'account');
+			ReqHandling::createRow($conn, "account");
 			
 			$_REQUEST["id"] = "KH" . strval(sprintf("%03d", $maxIdCustomer+1));
 			$_REQUEST["id_user"] = "USR" . strval(sprintf("%03d", $maxIdAccount+1));
-			ReqHandling::createRow($conn, 'customer');
+			ReqHandling::createRow($conn, "customer");
+			
 		}
 	}
 
@@ -116,18 +149,21 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
 // ? http://localhost/doan/admin/Server/customer/customer.php?id_user=USR015
 
-if ($_SERVER['REQUEST_METHOD'] === "PUT") {
+if ($_SERVER["REQUEST_METHOD"] === "PUT") {
 
-	if (!isset($_REQUEST["id_user"])) {
+	$data = json_decode(file_get_contents('php://input'), true);
+
+	if (!isset($data["id_user"])) {
 		echo "Please specify the id_user of ACCOUNT for deleting.";
 		exit();
 	}
 
+
 	// * delete from child table
-	ReqHandling::deleteRowWithProperty($conn, 'customer', 'id_user', $_REQUEST['id_user']);
+	ReqHandling::deleteRowWithProperty($conn, "customer", "id_user", $data["id_user"]);
 	
 	// * delete from parent table
-	ReqHandling::deleteRowWithProperty($conn, 'account', 'id_user', $_REQUEST['id_user']);
+	ReqHandling::deleteRowWithProperty($conn, "account", "id_user", $data["id_user"]);
 
 }
 
